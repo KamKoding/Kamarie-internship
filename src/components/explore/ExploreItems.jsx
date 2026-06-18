37b3f508c897ed4e6a755ff6d5dac48c9ff793ce
KamKoding <qkamarie@gmail.com>
2026-06-18 16:34:49 -0700

    explore paged complete

--- src/components/explore/ExploreItems.jsx
+++ src/components/explore/ExploreItems.jsx
@@ -1,22 +1,86 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import AuthorImage from "../../images/author_thumbnail.jpg";
 import nftImage from "../../images/nftImage.jpg";
+import axios from "axios";
+
+const Countdown = ({ expiryDate }) => {
+  const getTimeLeft = () => new Date(expiryDate).getTime() - Date.now();
+  const [timeLeft, setTimeLeft] = useState(getTimeLeft());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTimeLeft(getTimeLeft());
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [expiryDate]);
+
+  if (timeLeft <= 0) {
+    return <div className="de_countdown">Expired</div>;
+  }
+
+  const totalSeconds = Math.floor(timeLeft / 1000);
+  const days = Math.floor(totalSeconds / 86400);
+  const hours = Math.floor((totalSeconds % 86400) / 3600);
+  const minutes = Math.floor((totalSeconds % 3600) / 60);
+  const seconds = totalSeconds % 60;
+
+  return (
+    <div className="de_countdown">
+      {days > 0 && `${days}d `}
+      {hours}h {minutes}m {seconds}s
+    </div>
+  );
+};
+
+const INITIAL_VISIBLE = 8;
+const LOAD_MORE_STEP = 4;
 
 const ExploreItems = () => {
+  const [items, setItems] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [visibleCount, setVisibleCount] = useState(INITIAL_VISIBLE);
+  const [sortOption, setSortOption] = useState("");
+
+  useEffect(() => {
+    setLoading(true);
+    const baseUrl =
+      "https://us-central1-nft-cloud-functions.cloudfunctions.net/explore";
+    const url = sortOption ? `${baseUrl}?filter=${sortOption}` : baseUrl;
+
+    axios.get(url).then((response) => {
+      setItems(response.data);
+      setLoading(false);
+    });
+  }, [sortOption]);
+
+  useEffect(() => {
+    setVisibleCount(INITIAL_VISIBLE);
+  }, [sortOption]);
+
+  const handleLoadMore = (e) => {
+    e.preventDefault();
+    setVisibleCount((prev) => prev + LOAD_MORE_STEP);
+  };
+
+  const hasMore = visibleCount < items.length;
   return (
     <>
       <div>
-        <select id="filter-items" defaultValue="">
+        <select
+          id="filter-items"
+          value={sortOption}
+          onChange={(e) => setSortOption(e.target.value)}
+        >
           <option value="">Default</option>
           <option value="price_low_to_high">Price, Low to High</option>
           <option value="price_high_to_low">Price, High to Low</option>
           <option value="likes_high_to_low">Most liked</option>
         </select>
       </div>
-      {new Array(8).fill(0).map((_, index) => (
+      {items.slice(0, visibleCount).map((item) => (
         <div
-          key={index}
+          key={item.id}
           className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
           style={{ display: "block", backgroundSize: "cover" }}
         >
@@ -27,11 +91,11 @@ const ExploreItems = () => {
                 data-bs-toggle="tooltip"
                 data-bs-placement="top"
               >
-                <img className="lazy" src={AuthorImage} alt="" />
+                <img className="lazy" src={item.authorImage} alt="" />
                 <i className="fa fa-check"></i>
               </Link>
             </div>
-            <div className="de_countdown">5h 30m 32s</div>
+            <Countdown expiryDate={item.expiryDate} />
 
             <div className="nft__item_wrap">
               <div className="nft__item_extra">
@@ -51,28 +115,39 @@ const ExploreItems = () => {
                   </div>
                 </div>
               </div>
-              <Link to="/item-details">
-                <img src={nftImage} className="lazy nft__item_preview" alt="" />
+              <Link to={`/item-details/${item.authorId}`}>
+                <img
+                  src={item.nftImage}
+                  className="lazy nft__item_preview"
+                  alt=""
+                />
               </Link>
             </div>
             <div className="nft__item_info">
               <Link to="/item-details">
-                <h4>Pinky Ocean</h4>
+                <h4>{item.title}</h4>
               </Link>
-              <div className="nft__item_price">1.74 ETH</div>
+              <div className="nft__item_price">{item.price} ETH</div>
               <div className="nft__item_like">
                 <i className="fa fa-heart"></i>
-                <span>69</span>
+                <span>{item.likes}</span>
               </div>
             </div>
           </div>
         </div>
       ))}
-      <div className="col-md-12 text-center">
-        <Link to="" id="loadmore" className="btn-main lead">
-          Load more
-        </Link>
-      </div>
+      {hasMore && (
+        <div className="col-md-12 text-center">
+          <Link
+            to=""
+            id="loadmore"
+            className="btn-main lead"
+            onClick={handleLoadMore}
+          >
+            Load more
+          </Link>
+        </div>
+      )}
     </>
   );
 };
