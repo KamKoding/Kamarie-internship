400338d67352444ad6f193b9db597f39f36f450d
KamKoding <qkamarie@gmail.com>
2026-06-18 16:37:28 -0700

    skeleton loading state for explore page

--- src/components/explore/ExploreItems.jsx
+++ src/components/explore/ExploreItems.jsx
@@ -33,6 +33,46 @@ const Countdown = ({ expiryDate }) => {
   );
 };
 
+const SkeletonCard = () => (
+  <div className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12">
+    <div className="nft__item">
+      <div className="author_list_pp">
+        <span
+          className="skeleton-box"
+          style={{ width: 50, height: 50, borderRadius: "100%" }}
+        ></span>
+      </div>
+
+      <span
+        className="skeleton-box"
+        style={{ width: 70, height: 16, marginBottom: 10 }}
+      ></span>
+
+      <div className="nft__item_wrap">
+        <span
+          className="skeleton-box"
+          style={{ width: "100%", height: 220, borderRadius: 8 }}
+        ></span>
+      </div>
+
+      <div className="nft__item_info">
+        <span
+          className="skeleton-box"
+          style={{ width: "70%", height: 18, marginBottom: 10 }}
+        ></span>
+        <span
+          className="skeleton-box"
+          style={{ width: "40%", height: 14, marginBottom: 10 }}
+        ></span>
+        <span
+          className="skeleton-box"
+          style={{ width: "25%", height: 14 }}
+        ></span>
+      </div>
+    </div>
+  </div>
+);
+
 const INITIAL_VISIBLE = 8;
 const LOAD_MORE_STEP = 4;
 
@@ -78,64 +118,68 @@ const ExploreItems = () => {
           <option value="likes_high_to_low">Most liked</option>
         </select>
       </div>
-      {items.slice(0, visibleCount).map((item) => (
-        <div
-          key={item.id}
-          className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
-          style={{ display: "block", backgroundSize: "cover" }}
-        >
-          <div className="nft__item">
-            <div className="author_list_pp">
-              <Link
-                to="/author"
-                data-bs-toggle="tooltip"
-                data-bs-placement="top"
-              >
-                <img className="lazy" src={item.authorImage} alt="" />
-                <i className="fa fa-check"></i>
-              </Link>
-            </div>
-            <Countdown expiryDate={item.expiryDate} />
+      {loading
+        ? Array.from({ length: INITIAL_VISIBLE }).map((_, index) => (
+            <SkeletonCard key={index} />
+          ))
+        : items.slice(0, visibleCount).map((item) => (
+            <div
+              key={item.id}
+              className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
+              style={{ display: "block", backgroundSize: "cover" }}
+            >
+              <div className="nft__item">
+                <div className="author_list_pp">
+                  <Link
+                    to="/author"
+                    data-bs-toggle="tooltip"
+                    data-bs-placement="top"
+                  >
+                    <img className="lazy" src={item.authorImage} alt="" />
+                    <i className="fa fa-check"></i>
+                  </Link>
+                </div>
+                <Countdown expiryDate={item.expiryDate} />
 
-            <div className="nft__item_wrap">
-              <div className="nft__item_extra">
-                <div className="nft__item_buttons">
-                  <button>Buy Now</button>
-                  <div className="nft__item_share">
-                    <h4>Share</h4>
-                    <a href="" target="_blank" rel="noreferrer">
-                      <i className="fa fa-facebook fa-lg"></i>
-                    </a>
-                    <a href="" target="_blank" rel="noreferrer">
-                      <i className="fa fa-twitter fa-lg"></i>
-                    </a>
-                    <a href="">
-                      <i className="fa fa-envelope fa-lg"></i>
-                    </a>
+                <div className="nft__item_wrap">
+                  <div className="nft__item_extra">
+                    <div className="nft__item_buttons">
+                      <button>Buy Now</button>
+                      <div className="nft__item_share">
+                        <h4>Share</h4>
+                        <a href="" target="_blank" rel="noreferrer">
+                          <i className="fa fa-facebook fa-lg"></i>
+                        </a>
+                        <a href="" target="_blank" rel="noreferrer">
+                          <i className="fa fa-twitter fa-lg"></i>
+                        </a>
+                        <a href="">
+                          <i className="fa fa-envelope fa-lg"></i>
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                  <Link to={`/item-details/${item.authorId}`}>
+                    <img
+                      src={item.nftImage}
+                      className="lazy nft__item_preview"
+                      alt=""
+                    />
+                  </Link>
+                </div>
+                <div className="nft__item_info">
+                  <Link to="/item-details">
+                    <h4>{item.title}</h4>
+                  </Link>
+                  <div className="nft__item_price">{item.price} ETH</div>
+                  <div className="nft__item_like">
+                    <i className="fa fa-heart"></i>
+                    <span>{item.likes}</span>
                   </div>
                 </div>
               </div>
-              <Link to={`/item-details/${item.authorId}`}>
-                <img
-                  src={item.nftImage}
-                  className="lazy nft__item_preview"
-                  alt=""
-                />
-              </Link>
             </div>
-            <div className="nft__item_info">
-              <Link to="/item-details">
-                <h4>{item.title}</h4>
-              </Link>
-              <div className="nft__item_price">{item.price} ETH</div>
-              <div className="nft__item_like">
-                <i className="fa fa-heart"></i>
-                <span>{item.likes}</span>
-              </div>
-            </div>
-          </div>
-        </div>
-      ))}
+          ))}
       {hasMore && (
         <div className="col-md-12 text-center">
           <Link
