c1c18ed12ef40afcff4860908a7b3946979d7b14
KamKoding <qkamarie@gmail.com>
2026-06-18 15:28:55 -0700

    all skeleton loading states done

--- src/components/home/TopSellers.jsx
+++ src/components/home/TopSellers.jsx
@@ -3,6 +3,27 @@ import { Link } from "react-router-dom";
 import AuthorImage from "../../images/author_thumbnail.jpg";
 import axios from "axios";
 
+const SkeletonItem = () => (
+  <li>
+    <div className="author_list_pp">
+      <span
+        className="skeleton-box"
+        style={{ width: 50, height: 50, borderRadius: "100%" }}
+      ></span>
+    </div>
+    <div className="author_list_info">
+      <span
+        className="skeleton-box"
+        style={{ width: 110, height: 14, marginBottom: 8 }}
+      ></span>
+      <span
+        className="skeleton-box"
+        style={{ width: 60, height: 14 }}
+      ></span>
+    </div>
+  </li>
+);
+
 const TopSellers = () => {
   const [items, setItems] = useState([]);
   const [loading, setLoading] = useState(true);
@@ -30,24 +51,28 @@ const TopSellers = () => {
           </div>
           <div className="col-md-12">
             <ol className="author_list">
-              {items.map((item) => (
-                <li key={item.id}>
-                  <div className="author_list_pp">
-                    <Link to={`/author/${item.authorId}`}>
-                      <img
-                        className="lazy pp-author"
-                        src={item.authorImage}
-                        alt=""
-                      />
-                      <i className="fa fa-check"></i>
-                    </Link>
-                  </div>
-                  <div className="author_list_info">
-                    <Link to={`/author/${item.authorId}`}>{item.authorName}</Link>
-                    <span>{item.price}</span>
-                  </div>
-                </li>
-              ))}
+              {loading
+                ? Array.from({ length: 12 }).map((_, index) => (
+                    <SkeletonItem key={index} />
+                  ))
+                : items.map((item) => (
+                    <li key={item.id}>
+                      <div className="author_list_pp">
+                        <Link to={`/author/${item.authorId}`}>
+                          <img
+                            className="lazy pp-author"
+                            src={item.authorImage}
+                            alt=""
+                          />
+                          <i className="fa fa-check"></i>
+                        </Link>
+                      </div>
+                      <div className="author_list_info">
+                        <Link to={`/author/${item.authorId}`}>{item.authorName}</Link>
+                        <span>{item.price} ETH</span>
+                      </div>
+                    </li>
+                  ))}
             </ol>
           </div>
         </div>
