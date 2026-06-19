c9c371313734c37435577318605a8e1f1b3ed601
KamKoding <qkamarie@gmail.com>
2026-06-18 17:51:53 -0700

    author page complete

--- src/components/author/AuthorItems.jsx
+++ src/components/author/AuthorItems.jsx
@@ -3,59 +3,88 @@ import { Link } from "react-router-dom";
 import AuthorImage from "../../images/author_thumbnail.jpg";
 import nftImage from "../../images/nftImage.jpg";
 
-const AuthorItems = () => {
+const SkeletonCard = () => (
+  <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12">
+    <div className="nft__item">
+      <div className="author_list_pp">
+        <div className="skeleton-box skeleton-avatar" />
+      </div>
+      <div className="nft__item_wrap">
+        <div className="skeleton-box skeleton-image" />
+      </div>
+      <div className="nft__item_info">
+        <div className="skeleton-box skeleton-title" />
+        <div className="skeleton-box skeleton-subtitle" style={{ marginTop: "8px" }} />
+      </div>
+    </div>
+  </div>
+);
+
+
+const AuthorItems = ({ authorData }) => {
   return (
     <div className="de_tab_content">
       <div className="tab-1">
         <div className="row">
-          {new Array(8).fill(0).map((_, index) => (
-            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
-              <div className="nft__item">
-                <div className="author_list_pp">
-                  <Link to="">
-                    <img className="lazy" src={AuthorImage} alt="" />
-                    <i className="fa fa-check"></i>
-                  </Link>
-                </div>
-                <div className="nft__item_wrap">
-                  <div className="nft__item_extra">
-                    <div className="nft__item_buttons">
-                      <button>Buy Now</button>
-                      <div className="nft__item_share">
-                        <h4>Share</h4>
-                        <a href="" target="_blank" rel="noreferrer">
-                          <i className="fa fa-facebook fa-lg"></i>
-                        </a>
-                        <a href="" target="_blank" rel="noreferrer">
-                          <i className="fa fa-twitter fa-lg"></i>
-                        </a>
-                        <a href="">
-                          <i className="fa fa-envelope fa-lg"></i>
-                        </a>
+          {!authorData
+            ? Array.from({ length: 8 }).map((_, index) => (
+                <SkeletonCard key={index} />
+              ))
+            : authorData?.nftCollection?.map((nft) => (
+                <div
+                  className="col-lg-3 col-md-6 col-sm-6 col-xs-12"
+                  key={nft.id}
+                >
+                  <div className="nft__item">
+                    <div className="author_list_pp">
+                      <Link to={`/author/${authorData.authorId}`}>
+                        <img
+                          className="lazy"
+                          src={authorData.authorImage}
+                          alt=""
+                        />
+                        <i className="fa fa-check"></i>
+                      </Link>
+                    </div>
+                    <div className="nft__item_wrap">
+                      <div className="nft__item_extra">
+                        <div className="nft__item_buttons">
+                          <button>Buy Now</button>
+                          <div className="nft__item_share">
+                            <h4>Share</h4>
+                            <a href="" target="_blank" rel="noreferrer">
+                              <i className="fa fa-facebook fa-lg"></i>
+                            </a>
+                            <a href="" target="_blank" rel="noreferrer">
+                              <i className="fa fa-twitter fa-lg"></i>
+                            </a>
+                            <a href="">
+                              <i className="fa fa-envelope fa-lg"></i>
+                            </a>
+                          </div>
+                        </div>
+                      </div>
+                      <Link to={`item-details/${nft.nftId}`}>
+                        <img
+                          src={nft.nftImage}
+                          className="lazy nft__item_preview"
+                          alt=""
+                        />
+                      </Link>
+                    </div>
+                    <div className="nft__item_info">
+                      <Link to={`item-details/${nft.nftId}`}>
+                        <h4>{nft.title}</h4>
+                      </Link>
+                      <div className="nft__item_price">{nft.price} ETH</div>
+                      <div className="nft__item_like">
+                        <i className="fa fa-heart"></i>
+                        <span>{nft.likes}</span>
                       </div>
                     </div>
                   </div>
-                  <Link to="/item-details">
-                    <img
-                      src={nftImage}
-                      className="lazy nft__item_preview"
-                      alt=""
-                    />
-                  </Link>
                 </div>
-                <div className="nft__item_info">
-                  <Link to="/item-details">
-                    <h4>Pinky Ocean</h4>
-                  </Link>
-                  <div className="nft__item_price">2.52 ETH</div>
-                  <div className="nft__item_like">
-                    <i className="fa fa-heart"></i>
-                    <span>97</span>
-                  </div>
-                </div>
-              </div>
-            </div>
-          ))}
+              ))}
         </div>
       </div>
     </div>
--- src/pages/Author.jsx
+++ src/pages/Author.jsx
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import AuthorBanner from "../images/author_banner.jpg";
 import AuthorItems from "../components/author/AuthorItems";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import AuthorImage from "../images/author_thumbnail.jpg";
+import axios from "axios";
 
 const Author = () => {
+  const [author, setAuthor] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const { authorId } = useParams();
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://us-central1-nft-cloud-functions.cloudfunctions.net/authors?author=${authorId}`,
+      )
+      .then((response) => {
+        setAuthor(response.data);
+        setLoading(false);
+      });
+  }, [authorId]);
+
+  const [following, setFollowing] = useState(false);
+
+  const handleFollow = () => {
+    setFollowing((prev) => !prev);
+  };
+
   return (
     <div id="wrapper">
       <div className="no-bottom no-top" id="content">
@@ -15,7 +37,9 @@ const Author = () => {
           aria-label="section"
           className="text-light"
           data-bgimage="url(images/author_banner.jpg) top"
-          style={{ background: `url(${AuthorBanner}) top` }}
+          style={{
+            background: `url(${author?.authorImage || AuthorBanner}) top`,
+          }}
         ></section>
 
         <section aria-label="section">
@@ -25,29 +49,69 @@ const Author = () => {
                 <div className="d_profile de-flex">
                   <div className="de-flex-col">
                     <div className="profile_avatar">
-                      <img src={AuthorImage} alt="" />
+                      {loading ? (
+                        <div
+                          className="skeleton-box skeleton-avatar"
+                          style={{ width: "150px", height: "150px", borderRadius: "50%" }}
+                        />
+                      ) : (
+                        <img src={author?.authorImage} alt="" />
+                      )}
 
                       <i className="fa fa-check"></i>
                       <div className="profile_name">
                         <h4>
-                          Monica Lucas
-                          <span className="profile_username">@monicaaaa</span>
-                          <span id="wallet" className="profile_wallet">
-                            UDHUHWudhwd78wdt7edb32uidbwyuidhg7wUHIFUHWewiqdj87dy7
-                          </span>
-                          <button id="btn_copy" title="Copy Text">
-                            Copy
-                          </button>
+                          {loading ? (
+                            <>
+                              <div
+                                className="skeleton-box skeleton-title"
+                                style={{ width: "200px" }}
+                              />
+                              <div
+                                className="skeleton-box skeleton-subtitle"
+                                style={{ width: "120px", marginTop: "8px" }}
+                              />
+                              <div
+                                className="skeleton-box skeleton-subtitle"
+                                style={{ width: "300px", marginTop: "8px" }}
+                              />
+                            </>
+                          ) : (
+                            <>
+                              {author?.authorName}
+                              <span className="profile_username">
+                                @{author?.tag}
+                              </span>
+                              <span id="wallet" className="profile_wallet">
+                                {author?.address}
+                              </span>
+                              <button id="btn_copy" title="Copy Text">
+                                Copy
+                              </button>
+                            </>
+                          )}
                         </h4>
                       </div>
                     </div>
                   </div>
                   <div className="profile_follow de-flex">
                     <div className="de-flex-col">
-                      <div className="profile_follower">573 followers</div>
-                      <Link to="#" className="btn-main">
-                        Follow
-                      </Link>
+                      {loading ? (
+                        <div
+                          className="skeleton-box skeleton-subtitle"
+                          style={{ width: "80px" }}
+                        />
+                      ) : (
+                        <div className="profile_follower">
+                          {following
+                            ? (author?.followers || 0) + 1
+                            : author?.followers}{" "}
+                          followers
+                        </div>
+                      )}
+                      <button onClick={handleFollow} className="btn-main">
+                        {following ? "Following" : "Follow"}
+                      </button>
                     </div>
                   </div>
                 </div>
@@ -55,7 +119,7 @@ const Author = () => {
 
               <div className="col-md-12">
                 <div className="de_tab tab_simple">
-                  <AuthorItems />
+                  <AuthorItems authorData={author} />
                 </div>
               </div>
             </div>
