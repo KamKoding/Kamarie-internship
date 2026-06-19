4a401877c020413b7cc0f3a06bf2a0cc923d39bb
KamKoding <qkamarie@gmail.com>
2026-06-19 02:19:09 -0700

    all hrefs fixed

--- src/components/explore/ExploreItems.jsx
+++ src/components/explore/ExploreItems.jsx
@@ -15,7 +15,7 @@ const Countdown = ({ expiryDate }) => {
       setTimeLeft(getTimeLeft());
     }, 1000);
     return () => clearInterval(interval);
-  }, [expiryDate]);
+  }, [expiryDate, getTimeLeft]);
 
   if (timeLeft <= 0) {
     return <div className="de_countdown">Expired</div>;
@@ -162,13 +162,13 @@ const ExploreItems = () => {
                       <button>Buy Now</button>
                       <div className="nft__item_share">
                         <h4>Share</h4>
-                        <a href="" target="_blank" rel="noreferrer">
+                        <a href="#" onClick={(e) => e.preventDefault()} target="_blank" rel="noreferrer">
                           <i className="fa fa-facebook fa-lg"></i>
                         </a>
-                        <a href="" target="_blank" rel="noreferrer">
+                        <a href="#" onClick={(e) => e.preventDefault()} target="_blank" rel="noreferrer">
                           <i className="fa fa-twitter fa-lg"></i>
                         </a>
-                        <a href="">
+                        <a href="#" onClick={(e) => e.preventDefault()}>
                           <i className="fa fa-envelope fa-lg"></i>
                         </a>
                       </div>
